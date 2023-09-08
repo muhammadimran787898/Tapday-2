97b2da0fb3f7317e72947eee6fb92bfa50c6179f
muhammad <munir787898@gmail.com>
2023-09-08 14:54:49 +0500

    Push existing project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,49 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    NEXT_APP_SERVER: process.env.NEXT_APP_SERVER,
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/loading",
+        permanent: true,
+      },
+      {
+        source: "/setting",
+        destination: "/loading?page=setting",
+        permanent: true,
+      },
+      {
+        source: "/engage",
+        destination: "/loading?page=engage",
+        permanent: true,
+      },
+      // integrations
+      {
+        source: "/integrations",
+        destination: "/loading?page=integrations",
+        permanent: true,
+      },
+      {
+        source: "/analytics",
+        destination: "/loading?page=analytics",
+        permanent: true,
+      },
+    ];
+  },
+  reactStrictMode: false,
+  swcMinify: true,
+  images: {
+    domains: ["cdn.shopify.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,113 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}'
+    // './src/components/**/*.{js,ts,jsx,tsx}'
+    // './src/components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      width: {
+        68: '17rem',
+        76: '19rem'
       },
-    },
+      animation: {
+        marquee: 'marquee 25s linear infinite'
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' }
+        }
+      },
+      colors: {
+        primary: '#0088f5',
+        'primary-light': '#e5f3fe',
+        secondary: '#aeaeb2',
+        yellow: '#ffb901',
+        'widget-border': 'rgb(150 150 150 / 0.2)',
+        'widget-border-2': 'rgb(150 150 150 / 0.5)',
+        orange: '#f13800',
+        drakred: '#FF0000',
+        'green-col': '#138736',
+        dark: '#767676',
+        grey: '#848484',
+        'secondary-dark': '#aeaeb280',
+        'secondary-light': '#fcfcfc',
+        'secondary-black': 'rgba(0, 0, 0, 0.30)',
+        main: '#26292D',
+        inner: '#282B2F',
+        setting: '#469E9B',
+        borderColor: 'rgba(255, 255, 255, 0.08)',
+        transparentBg: 'rgba(255, 255, 255, 0.03)',
+        // 'light-black': '#1a1a1a',
+        'light-black': '#969696',
+        skeletonClr: '#a2a2a259',
+        innerBg: '#cce7fd',
+        engage: '#4E28CD'
+      },
+      fontFamily: {
+        medium: 'sofia-medium',
+        sofiaSemi: 'sofia-semi',
+        regular: 'sofia-regular',
+        sofiaBold: 'sofia-bold',
+        sofiaLight: 'sofia-light',
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+        mono: ['Menlo', 'monospace'],
+        sofiapro: ['Sofia Pro']
+      },
+      container: {
+        center: true,
+        padding: '1rem'
+      },
+      screens: {
+        xs: '480px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        dxl: '1300px',
+        '8xl': '1440px',
+
+        xxl: '2000px'
+      },
+      maxWidth: {
+        xxl: '2000px'
+      },
+      borderRadius: {
+        none: '0',
+        sm: '0.125rem',
+        base: '4px',
+        main: '0.7rem',
+        md: '3px',
+        lg: '0.5rem',
+        full: '9999px'
+      },
+      boxShadow: {
+        main: '0px 0px 73px 27px #9d949490',
+        secondary: '2px 2px 20px 0px #a2a2a2',
+        menuItem: 'inset 0 0 0 1px #0088F5',
+        'setting-gray': '0px 1px 6px #d0d0d0',
+        product: '0 2px 2px rgba(147,158,171,.3)',
+        'product-hover': '0 4px 8px rgba(147,158,171,.5)'
+      },
+      scale: {
+        10: '0.1',
+        20: '0.2',
+        35: '0.35',
+        40: '0.4'
+      }
+    }
   },
-  plugins: [],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true
+  },
+  corePlugins: {
+    transition: true,
+    transform: true
+  },
+  plugins: []
 }
-export default config
