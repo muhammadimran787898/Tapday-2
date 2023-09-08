13ff89f233350fa39671ca9c8c1c8f07f50b391c
muhammad <munir787898@gmail.com>
2023-09-08 15:43:11 +0500

    feat(Page): title change

--- app/page.tsx
+++ app/page.tsx
@@ -69,7 +69,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            Learn about next js
           </p>
         </a>
 
