d9ef32e5e81d3c59bbf3af600abb2ae07b267274
muhammad <munir787898@gmail.com>
2023-09-11 16:58:30 +0500

    feat(Page): title change

--- next.config.js
+++ next.config.js
@@ -1,49 +1,56 @@
 /** @type {import('next').NextConfig} */
+const { i18n } = require('./i18n')
 const nextConfig = {
   env: {
-    NEXT_APP_SERVER: process.env.NEXT_APP_SERVER,
+    NEXT_APP_SERVER: process.env.NEXT_APP_SERVER
   },
+  i18n: {
+    locales: ['en-US', 'fr-FR', 'es-ES'],
+
+    defaultLocale: 'en-US'
+  },
+
   async redirects() {
     return [
       {
-        source: "/",
-        destination: "/loading",
-        permanent: true,
+        source: '/',
+        destination: '/loading',
+        permanent: true
       },
       {
-        source: "/setting",
-        destination: "/loading?page=setting",
-        permanent: true,
+        source: '/setting',
+        destination: '/loading?page=setting',
+        permanent: true
       },
       {
-        source: "/engage",
-        destination: "/loading?page=engage",
-        permanent: true,
+        source: '/engage',
+        destination: '/loading?page=engage',
+        permanent: true
       },
       // integrations
       {
-        source: "/integrations",
-        destination: "/loading?page=integrations",
-        permanent: true,
+        source: '/integrations',
+        destination: '/loading?page=integrations',
+        permanent: true
       },
       {
-        source: "/analytics",
-        destination: "/loading?page=analytics",
-        permanent: true,
-      },
-    ];
+        source: '/analytics',
+        destination: '/loading?page=analytics',
+        permanent: true
+      }
+    ]
   },
   reactStrictMode: false,
   swcMinify: true,
   images: {
-    domains: ["cdn.shopify.com"],
+    domains: ['cdn.shopify.com'],
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
-};
+        protocol: 'https',
+        hostname: '**'
+      }
+    ]
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
